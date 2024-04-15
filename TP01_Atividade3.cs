f1381a18be0c7f7485a2f4270af9e6aed525cf84
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2024-04-15 19:11:43 -0300

    Add files via upload

---
+++ TP01_Atividade3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Program {
+    public static void Main (string[] args) {
+      
+      string[] posExe = Console.ReadLine().Split(' ');
+
+        int N = int.Parse(posExe[0]);
+        int M = int.Parse(posExe[1]);
+
+        if (N > 0 && N <= 200000 && M > 0 && M <= 200000) {
+            int[] vetor = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                vetor[i] = 0;
+            }
+
+            for (int i = 0; i < M; i++) {
+                string valores = Console.ReadLine();
+                string[] posicao = valores.Split(' ');
+
+              
+                int comando = int.Parse(posicao[0]);
+                int indice = int.Parse(posicao[1])-1;
+                int valor = 0;
+              
+                if (posicao.Length == 3)
+                {
+                  valor = int.Parse(posicao[2]);
+                }
+
+              
+
+
+                if (comando == 1) {
+                    for (int j = 0; j < valor && (indice+j)<N; j++) {
+                        vetor[indice + j] = vetor[indice + j] + Convert.ToInt32(posicao[2]);
+                        posicao[2] = Convert.ToString(Convert.ToInt32(posicao[2]) - 1);
+                    }
+                }
+                else if (comando == 2) {
+                    for (int j = 0; j < valor  && indice-j>=0; j++) {
+                      vetor[indice - j] = vetor[indice - j] + Convert.ToInt32(posicao[2]);
+                      posicao[2] = Convert.ToString(Convert.ToInt32(posicao[2]) - 1);
+                    }
+                }
+                else if (comando == 3) {
+
+                   Console.WriteLine(vetor[indice]);
+
+                }
+              /*for (int x = 0; x < N; x++) {
+                Console.WriteLine(vetor[x]);
+              }*/     
+            }
+        }
+    }
+}
---
+++ TP01_Atividade4.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int E = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < E; i++)
+        {
+            int D = int.Parse(Console.ReadLine());
+            int sensor = DeterminarSensor(D);
+            Console.WriteLine(sensor);
+        }
+    }
+    static int DeterminarSensor(int D)
+    {
+        int posicao = (D - 1) % 8 + 1;
+
+        if (posicao == 1 || posicao == 7)
+            return 2;
+        else if (posicao == 2 || posicao == 6)
+            return 1;
+        else
+            return 3;
+    }
+}

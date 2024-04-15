e700edff6a5a1fe241cc1b0b1a1c12f1bde043c1
Rafael Tanure <91788550+RafaelTanure@users.noreply.github.com>
2024-04-15 19:11:17 -0300

    Add files via upload

---
+++ TP01_Atividade1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int L = int.Parse(Console.ReadLine());
+        int C = int.Parse(Console.ReadLine());
+
+        int tipo1 = (L * C) + ((L - 1) * (C - 1));
+        int tipo2 = 2 * (L - 1) + 2 * (C - 1);
+
+        Console.WriteLine(tipo1);
+        Console.WriteLine(tipo2);
+    }
+}
---
+++ TP01_Atividade2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] sizes = Console.ReadLine().Split();
+        int M = int.Parse(sizes[0]);
+        int N = int.Parse(sizes[1]);
+
+        int[,] matriz = new int[M, N];
+
+        for (int i = 0; i < M; i++)
+        {
+            string[] linha = Console.ReadLine().Split();
+            for (int j = 0; j < N; j++)
+            {
+                matriz[i, j] = int.Parse(linha[j]);
+            }
+        }
+
+        bool simetrica = VerificarSimetria(matriz, M, N);
+
+        Console.WriteLine(simetrica ? 1 : 0);
+    }
+
+    static bool VerificarSimetria(int[,] matriz, int M, int N)
+    {
+        if (M != N)
+            return false;
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (matriz[i, j] != matriz[j, i])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
